db869036430b4201a9327558a9f617a518b77664
Roman <63170330+RomaYaki@users.noreply.github.com>
2021-08-04 17:10:28 +0300

    t2

--- src/Page/PicPage.js
+++ src/Page/PicPage.js
@@ -5,15 +5,16 @@ import { API_ROOT } from '../constants/api';
 const PickList = () => {
     const getResource = async (url) => {
         const body = await getApiResource(url);
-        console.log(body);
-        console.log(`url`, url)
+        console.log(`body`, body)
     }
 
     useEffect(() => {
         getResource(API_ROOT)
-        console.log(`API_ROOT`, API_ROOT)
     }, []);
-    return <>PickList</>
+    
+    return (
+        <>PickList</>
+    )
 }
 
 export default PickList;
--- src/utils/network.js
+++ src/utils/network.js
@@ -2,13 +2,14 @@
 export const getApiResource = async (url) => {
     try {
         const res = await fetch(url);
+        console.log(`res`, res)
 
         if (!res.ok) {
             console.error('Could not fetch.', res.status);
             return false;
         }
 
-        return await res.json(); 
+        return await res; 
         } catch (error) {
         console.error('Could not fetch.', error.message);
         return false;
